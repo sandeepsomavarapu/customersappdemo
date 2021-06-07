90c98502c2e2f996e466a4e7411b61c716635317
SANDEEP <SANDEEP@DESKTOP-7G2JT9N>
2021-06-07 15:30:35 +0530

    after adding comments commit....

--- customermsapp/src/main/java/com/cts/employee/CustomermsappApplication.java
+++ customermsapp/src/main/java/com/cts/employee/CustomermsappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomermsappApplication {
-
+//added some comments
 	public static void main(String[] args) {
 		SpringApplication.run(CustomermsappApplication.class, args);
 	}
