9721f9cbe7b9ec1344db7937784bdd6c0a5609fc
SANDEEP <SANDEEP@DESKTOP-7G2JT9N>
2021-06-07 15:28:17 +0530

    first commit

---
+++ customermsapp/src/main/java/com/cts/employee/CustomermsappApplication.java
@@ -0,0 +1,13 @@
+package com.cts.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomermsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomermsappApplication.class, args);
+	}
+
+}
